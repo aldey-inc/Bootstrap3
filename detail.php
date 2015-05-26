d9aa9b99937768f8e28c6bd8c8ea61b4390808a6
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-05-25 22:35:54 -0500

    Agregación de formadores y lista de detalles
    Se agregaron los formadores y la agenda del curso

--- detail.php
+++ detail.php
@@ -8,8 +8,96 @@
 </head>
 <body>
 	<?php include 'inc/header.php' ?>
-	<h1>Bootstrap Ejemplo</h1>
-	<p>Hola mundo.</p>
+	
+	<article id="w1-detail" class="detail">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<h2>Desarrolla front-end like a boss prendiendo BootStrap 3</h2>
+					</div>
+				</div>
+			</div>
+		</header>		
+	</article>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-7">
+				<section id="intro">
+					<h3>En este workshop aprenderas...</h3>
+					<p>
+					 	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere amet, nihil earum hic, quis labore possimus, ipsa aspernatur sint velit officiis placeat quod, cumque quibusdam. Dicta molestiae sit numquam excepturi.
+					 	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum assumenda, minus fugit voluptate, laboriosam inventore. Vel, dolorem accusantium in magnam dicta sit necessitatibus expedita quisquam maiores cumque molestias, illum ab!
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, officiis natus. Eum ducimus consequatur quibusdam id, mollitia fugit velit quidem, suscipit nulla perferendis sunt tempore vel libero impedit sequi repellat!	
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem deleniti fugit nam totam voluptates facilis, ab molestiae perferendis esse illo iste ratione earum, nemo ad fugiat dolores, harum ipsum. Hic!
+					</p>
+				</section>
+				<section id="trainer-list">
+					<h3>Los formadores</h3>
+					<div class="trainer row">
+						<div class="col-xs-4">
+							<img src="img/marcos_profile.png" alt="Marcos González">
+						</div>
+						<div class="col-xs-8">
+							<h4>Marco González</h4>
+							<p>
+								Fundador y directo de [Q] eractiva <br />
+								Co-fundador y CTO de Infocket. <br />
+								Formador en video2brain.
+							</p>
+							<a href="http://about.me/qmarcos" class="trainer-link">CV Online</a>
+							<a href="https://twitter.com/qmarcos" class="trainer-link"@qmarcos></a>
+						</div>
+					</div>
+					<div class="trainer row">
+						<div class="col-xs-4">
+							<img src="img/armando_profile.png" alt="Armando Broncas">
+						</div>
+						<div class="col-xs-8">
+							<h4>Armando Broncas</h4>
+							<p>
+								Fundador y directo de [Q] eractiva <br />
+								Co-fundador y CTO de Infocket. <br />
+								Formador en video2brain.
+							</p>
+							<a href="http://about.me/qmarcos" class="trainer-link">CV Online</a>
+							<a href="https://twitter.com/qmarcos" class="trainer-link"@qmarcos></a>
+						</div>
+					</div>
+				</section>
+			</div>
+			<div class="col-xs-5">
+				<section id="registration">
+					<h3>Registro</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda perferendis nemo deleniti magnam unde natus, sed aliquid voluptatum quod consectetur aliquam quis aut a tempore ratione fugiat, provident distinctio quam.
+						
+					</p>
+					<div id="registration-badge"></div>
+				</section>
+				<section id="agenda">
+					<h3>Agenda del día</h3>
+					<h4><span>W21</span> Sabado 29 de mayo</h4>
+					<dl class="dl-horizontal">
+						<dt>10,00</dt>
+						<dd>WorkShop</dd>
+						<dt>11,00</dt>
+						<dd>Break</dd>
+						<dt>12,00</dt>
+						<dd>WorkShop 2</dd>
+						<dt>13,00</dt>
+						<dd>Break 2</dd>
+						<dt>14,00</dt>
+						<dd>WorkShop 3</dd>
+						<dt>15,00</dt>
+						<dd>Find del curso</dd>
+					</dl>
+				</section>
+			</div>
+		</div>
+	</div>
+
 	<?php include 'inc/footer.php' ?>
 	<?php include 'inc/footer_common.php' ?>
 </body>
