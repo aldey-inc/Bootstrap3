5a9ca8dfb34c9421ee2ff8d9fab30f049bbc6cf1
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-05-31 16:46:31 -0500

    Se adecuo la parte del pie para poder mostrarlo de manera correcta en todos los contextos

--- inc/footer.php
+++ inc/footer.php
@@ -1,24 +1,24 @@
 <footer>
 <div class="container">
 	<div class="row">
-		<div class="col-xs-5">
+		<div class="col-xs-12 col-sm-5">
 			<h3>Proximos WorkShops</h3>
 			<ul id="next-workshops">
 				<li><a href="detail.php">Titulo del primer workshop de prueba</a></li>
 				<li><a href="detail.php">Titulo del segundo workshop de prueba</a></li>
 			</ul>
 		</div>
-		<div class="col-xs-6 col-xs-offset-1">
+		<div class="col-xs-12 col-sm-7 col-md-6 col-md-offset-1">
 			<h3>No te pierdas nuestras novedades</h3>
 			<form id="subscribe-form" action="">
 				<div class="row">
-					<div class="col-xs-8">
+					<div class="col-xs-7 col-sm-8">
 						<div class="form-group">
 							<label for="email" class="sr-only">Tu correo electrónico</label>
 							<input type="text" id="email" name="email" class="form-control input-lg" placeholder="tu correo electronico">
 						</div>
 					</div>
-					<div class="col-xs-4">
+					<div class="col-xs-5 col-sm-4">
 						<button class="btn btn-custom btn-lg">Suscribirme</button>
 					</div>
 				</div>
@@ -27,11 +27,11 @@
 		<div class="clearfix">
 			
 		</div>
-		<div class="col-xs-5">
+		<div class="col-xs-12 col-sm-5">
 			<h3>Sobre los WorkShops</h3>
 			<p>Estos workshops son el resultado  la suma de la ilusion y esfuerzos de <a href="#">	Empresa </a> Una apuesta informatica nueva del panorama informativo</p>
 		</div>
-		<div class="col-xs-6 col-xs-offset-1">
+		<div class="col-xs-12 col-sm-7 col-md-6 col-md-offset-1">
 			<h3>¿Te gusta? Corre la voz</h3>
 			<p>Si te gustan los cursos dalos a conocer en todo lugar y te estaremos agradecidos y tambien si te interesa con gusto podemos dar los cursos que tu</p>
 		</div>
